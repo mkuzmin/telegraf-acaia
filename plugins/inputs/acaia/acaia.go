35bf6d6a4e2545a2d36b12689cfd71858f1ae3b7
Michael Kuzmin <michael.kuzmin@jetbrains.com>
2023-11-19 13:13:11 +0200

    Pearl S support, messages can be split into multiple notifications.

--- plugins/inputs/acaia/acaia.go
+++ plugins/inputs/acaia/acaia.go
@@ -1,15 +1,12 @@
 package acaia
 
 import (
-	"bytes"
 	"encoding/binary"
 	"errors"
-	"fmt"
 	"github.com/influxdata/telegraf"
 	"github.com/influxdata/telegraf/plugins/inputs"
 	"strings"
 	"sync"
-	"time"
 	"tinygo.org/x/bluetooth"
 )
 
@@ -24,13 +21,14 @@ var (
 )
 
 type AcaiaInput struct {
-	Name string `toml:"name"`
+	Model string `toml:"model"`
 
-	acc    telegraf.Accumulator
-	wg     sync.WaitGroup
-	device *bluetooth.Device
-	weight chan float64
-	Log    telegraf.Logger `toml:"-"`
+	acc        telegraf.Accumulator
+	wg         sync.WaitGroup
+	device     *bluetooth.Device
+	byteChan   chan byte
+	weightChan chan float64
+	Log        telegraf.Logger `toml:"-"`
 }
 
 func (*AcaiaInput) SampleConfig() string {
@@ -38,8 +36,8 @@ func (*AcaiaInput) SampleConfig() string {
 }
 
 func (s *AcaiaInput) Init() error {
-	if s.Name == "" {
-		return errors.New("name is not set")
+	if s.Model == "" {
+		return errors.New("model is not set")
 	}
 
 	return nil
@@ -54,7 +52,7 @@ func (s *AcaiaInput) Start(acc telegraf.Accumulator) error {
 
 	ch := make(chan bluetooth.ScanResult, 1)
 	err = adapter.Scan(func(adapter *bluetooth.Adapter, result bluetooth.ScanResult) {
-		if strings.HasPrefix(result.AdvertisementPayload.LocalName(), s.Name) {
+		if strings.HasPrefix(result.AdvertisementPayload.LocalName(), s.Model) {
 			err = adapter.StopScan()
 			if err != nil {
 				return
@@ -72,26 +70,62 @@ func (s *AcaiaInput) Start(acc telegraf.Accumulator) error {
 		return err
 	}
 
-	services, err := s.device.DiscoverServices([]bluetooth.UUID{bluetooth.ServiceUUIDInternetProtocolSupport})
-	if err != nil {
-		return err
-	}
-	if len(services) == 0 {
-		return errors.New("could not find IP service")
-	}
-	service := services[0]
+	var readChar, writeChar bluetooth.DeviceCharacteristic
+	switch s.Model {
+	case "ACAIAL1":
+		serviceUUID := bluetooth.ServiceUUIDInternetProtocolSupport
+		readCharUUID := bluetooth.New16BitUUID(0x2A80)
+		services, err := s.device.DiscoverServices([]bluetooth.UUID{serviceUUID})
+		if err != nil {
+			return err
+		}
+		if len(services) != 1 {
+			return errors.New("could not find IP service")
+		}
+		service := services[0]
+		s.Log.Debug("service: ", service.UUID().String())
 
-	chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{bluetooth.New16BitUUID(0x2A80)})
-	if err != nil {
-		return err
-	}
-	if len(chars) == 0 {
-		return errors.New("could not find characteristic")
-	}
-	char := chars[0]
+		chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{readCharUUID})
+		if err != nil {
+			return err
+		}
+		if len(chars) != 1 {
+			return errors.New("could not find characteristic")
+		}
+		readChar = chars[0]
+		writeChar = readChar
 
-	s.weight = make(chan float64)
-	err = char.EnableNotifications(s.readEvent)
+	case "PEARLS":
+		serviceUUID, _ := bluetooth.ParseUUID("49535343-fe7d-4ae5-8fa9-9fafd205e455")
+		readCharUUID, _ := bluetooth.ParseUUID("49535343-1e4d-4bd9-ba61-23c647249616")
+		writeCharUUID, _ := bluetooth.ParseUUID("49535343-8841-43f4-a8d4-ecbe34729bb3")
+		services, err := s.device.DiscoverServices([]bluetooth.UUID{serviceUUID})
+		if err != nil {
+			return err
+		}
+		if len(services) != 1 {
+			return errors.New("could not find IP service")
+		}
+		service := services[0]
+		s.Log.Debug("service: ", service.UUID().String())
+
+		chars, err := service.DiscoverCharacteristics([]bluetooth.UUID{readCharUUID, writeCharUUID})
+		if err != nil {
+			return err
+		}
+		if len(chars) != 2 {
+			return errors.New("could not find characteristics")
+		}
+		readChar = chars[0]
+		writeChar = chars[1]
+	}
+	s.Log.Debug("read char: ", readChar.UUID().String())
+	s.Log.Debug("write char: ", writeChar.UUID().String())
+
+	s.byteChan = make(chan byte)
+	s.weightChan = make(chan float64)
+	go s.processBytes()
+	err = readChar.EnableNotifications(s.readEvent)
 	if err != nil {
 		return err
 	}
@@ -116,27 +150,22 @@ func (s *AcaiaInput) Start(acc telegraf.Accumulator) error {
 		0x15, 0x06, // checksum
 	}
 
-	_, err = char.WriteWithoutResponse(auth)
+	_, err = writeChar.WriteWithoutResponse(auth)
 	if err != nil {
 		return err
 	}
 
-	_, err = char.WriteWithoutResponse(config)
+	_, err = writeChar.WriteWithoutResponse(config)
 	if err != nil {
 		return err
 	}
 
-	s.Log.Info("authenticated")
-	//go s.ping(char)
+	s.Log.Debug("authenticated")
+	//go s.ping(readChar)
 
-	//select {
-	//case data := <-s.weight:
-	//	fields := map[string]interface{}{"value": data}
-	//	acc.AddFields("weight", fields, nil)
-	//}
 	go func() {
 		for {
-			data := <-s.weight
+			data := <-s.weightChan
 			fields := map[string]interface{}{"value": data}
 			acc.AddFields("weight", fields, nil)
 		}
@@ -153,45 +182,83 @@ func (s *AcaiaInput) Gather(_ telegraf.Accumulator) error {
 	return nil
 }
 
-func (s *AcaiaInput) ping(char bluetooth.DeviceCharacteristic) {
-	for {
-		s.Log.Info("ping...")
-		char.WriteWithoutResponse(
-			[]byte{
-				0xEF, 0xDD,
-				0x00,
-				0x02, 0x00,
-				0x02, 0x00,
-			},
-		)
-		time.Sleep(5 * time.Second)
+//func (s *AcaiaInput) ping(char bluetooth.DeviceCharacteristic) {
+//	for {
+//		s.Log.Info("ping...")
+//		char.WriteWithoutResponse(
+//			[]byte{
+//				0xEF, 0xDD,
+//				0x00,
+//				0x02, 0x00,
+//				0x02, 0x00,
+//			},
+//		)
+//		time.Sleep(5 * time.Second)
+//	}
+//}
+
+func (s *AcaiaInput) readEvent(buf []byte) {
+	s.Log.Debugf("event: %x", buf)
+	for _, b := range buf {
+		s.byteChan <- b
 	}
 }
 
-func (s *AcaiaInput) readEvent(buf []byte) {
-	//s.Log.Info("read event")
-	if !bytes.HasPrefix(buf, []byte{0x08, 0x05}) {
-		//fmt.Printf("%x\n", buf)
-		return
+func (s *AcaiaInput) processBytes() {
+	s.Log.Debug("process bytes")
+	for {
+		b := <-s.byteChan
+		s.Log.Debugf("byte: %x", b)
+		if b != 0xEF {
+			continue
+		}
+		b = <-s.byteChan
+		s.Log.Debugf("byte: %x", b)
+		if b != 0xDD {
+			continue
+		}
+		b = <-s.byteChan
+		s.Log.Debugf("byte: %x", b)
+		if b != 0x0C {
+			continue
+		}
+
+		b = <-s.byteChan
+		s.Log.Debugf("byte: %x", b)
+		payload := []byte{b}
+		size := int(payload[0])
+		for i := 0; i < size-1; i++ {
+			b = <-s.byteChan
+			s.Log.Debugf("byte: %x", b)
+			payload = append(payload, b)
+		}
+		s.Log.Debugf("payload: %x", payload)
+
+		_ = <-s.byteChan // crc1
+		_ = <-s.byteChan // crc2
+		// TODO: check crc
+
+		if size != 8 {
+			continue
+		}
+
+		result := binary.LittleEndian.Uint16(payload[2:4])
+
+		var precision float64
+		switch payload[6] {
+		case 0x01:
+			precision = 10.0
+		case 0x02:
+			precision = 100.0
+		}
+
+		var negative float64
+		if payload[7]&0x02 == 0 {
+			negative = 1.0
+		} else {
+			negative = -1.0
+		}
+
+		s.weightChan <- negative * float64(result) / precision
 	}
-
-	//fmt.Printf("%x\n", buf)
-	var result int32
-
-	err := binary.Read(bytes.NewBuffer(buf[2:6]), binary.LittleEndian, &result)
-	if err != nil {
-		fmt.Println(err)
-	}
-
-	var negative float64
-	if buf[7]&0x02 == 0 {
-		negative = 1.0
-	} else {
-		negative = -1.0
-	}
-
-	weight := negative * float64(result) / 100.0
-
-	s.Log.Info("weight: ", weight)
-	s.weight <- weight
 }
